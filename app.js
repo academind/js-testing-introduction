f137288f0ed8ec14a787f8bb30d5a997f3c43139
Maximilian Schwarzmüller <company@academind.com>
2018-10-01 23:22:12 +0200

    added validation

--- app.js
+++ app.js
@@ -17,11 +17,33 @@ const createElement = (type, text, className) => {
   return newElement;
 };
 
+const validateInput = (text, notEmpty, isNumber) => {
+  // Validate user input with two pre-defined rules
+  if (!text) {
+    return false;
+  }
+  if (notEmpty && text.trim().length === 0) {
+    return false;
+  }
+  if (isNumber && +text === NaN) {
+    return false;
+  }
+  return true;
+};
+
 const addUser = () => {
   // Fetches the user input, creates a new HTML element based on it
   // and appends the element to the DOM
   const newUserNameInput = document.querySelector('input#name');
   const newUserAgeInput = document.querySelector('input#age');
+
+  if (
+    !validateInput(newUserNameInput.value, true, false) ||
+    !validateInput(newUserAgeInput.value, false, true)
+  ) {
+    return;
+  }
+
   const userList = document.querySelector('.user-list');
   const outputText = generateText(
     newUserNameInput.value,
