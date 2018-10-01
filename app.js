a23d715302c753bd0ce38a62c5397f86713b1560
Maximilian Schwarzmüller <company@academind.com>
2018-10-01 23:17:07 +0200

    starting setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const initApp = () => {
+  // Initializes the app, registers the button click listener
+  const newUserButton = document.querySelector('#btnAddUser');
+  newUserButton.addEventListener('click', addUser);
+};
+
+const generateText = (name, age) => {
+  // Returns output text
+  return `${name} (${age} years old)`;
+};
+
+const createElement = (type, text, className) => {
+  // Creates a new HTML element and returns it
+  const newElement = document.createElement(type);
+  newElement.classList.add(className);
+  newElement.textContent = text;
+  return newElement;
+};
+
+const addUser = () => {
+  // Fetches the user input, creates a new HTML element based on it
+  // and appends the element to the DOM
+  const newUserNameInput = document.querySelector('input#name');
+  const newUserAgeInput = document.querySelector('input#age');
+  const userList = document.querySelector('.user-list');
+  const outputText = generateText(
+    newUserNameInput.value,
+    newUserAgeInput.value
+  );
+  const element = createElement('li', outputText, 'user-item');
+  userList.appendChild(element);
+};
+
+// Start the app!
+initApp();
---
+++ styles.css
@@ -0,0 +1,46 @@
+body {
+  font-family: sans-serif;
+}
+
+.control-panel {
+  width: 25rem;
+}
+
+.input-container {
+  margin: 1rem;
+}
+
+.input-container label,
+.input-container input {
+  display: block;
+  width: 100%;
+}
+
+.input-container input {
+  font: inherit;
+}
+
+.button {
+  font: inherit;
+  background: #521751;
+  color: white;
+  border: 1px solid #521751;
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  margin: 0 1rem;
+}
+
+.user-list {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  margin: 1rem;
+  padding: 0;
+}
+
+.user-item {
+  margin-bottom: 1rem;
+  padding: 1rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  width: 20rem;
+}
