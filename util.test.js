5ea7302122e129fef536be9ad8847a999fc449df
Maximilian Schwarzmüller <company@academind.com>
2018-10-02 10:13:02 +0200

    added e2e test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
@@ -7,5 +8,24 @@ test('should output name and age', () => {
 
 test('should generate a valid text output', () => {
   const text = checkAndGenerate('Max', 29);
-  expect(text).toBe('Max (29 years old)')
+  expect(text).toBe('Max (29 years old)');
 });
+
+test('should create an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'file:///Users/mschwarzmueller/development/teaching/youtube/js-testing/index.html'
+  );
+  await page.click('input#name');
+  await page.type('input#name', 'Anna');
+  await page.click('input#age');
+  await page.type('input#age', '28');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
