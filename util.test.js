0dde1d74f880fb99526e4068bbc60c49b7337597
Joydip Roy <joydipand@gmail.com>
2018-10-19 13:41:09 +0530

    refactor util.test

--- util.test.js
+++ util.test.js
@@ -3,7 +3,8 @@
 const { loadTitle } = require('./util');
 
 test('should print an uppercase text', () => {
-  loadTitle().then(title => {
+  expect.assertions(1);
+  return loadTitle().then(title => {
     expect(title).toBe('DELECTUS AUT AUTEM');
   });
 });
